58be68ef885e93d5dfdf51cdaf14bdc41df2e9cb
Manuel Quiñones <manuel.por.aca@gmail.com>
2024-09-03 07:42:43 -0300

    Improvements

--- main-menu/main.gd
+++ main-menu/main.gd
@@ -7,3 +7,11 @@ func _on_demo_01_button_pressed() -> void:
 
 func _on_demo_02_button_pressed() -> void:
 	Global.change_scene("res://game-02/main.tscn")
+
+
+func _on_demo_03_button_pressed() -> void:
+	Global.change_scene("res://game-03/main.tscn")
+
+
+func _on_demo_04_button_pressed() -> void:
+	Global.change_scene("res://game-04/main.tscn")
